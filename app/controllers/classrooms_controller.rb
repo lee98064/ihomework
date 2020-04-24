32394811f75abcf8194eada0346686930f092cbf
Ryan Lee <lee1040316@gmail.com>
2020-04-24 09:59:37 +0800

    修改所有教室部分頁面

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,6 +1,7 @@
 class  ClassroomsController < ApplicationController
 	layout "classroom"
 	def index
+		@classrooms = Classroom.all		
 		# 暫放
 		# p Rails.application.eager_load!
 		# p ActiveRecord::Base.descendants
